59185731ac4358688dabb57a659bbaf0ddd197b9
rspaimoc2 <raduspaimoc@gmail.com>
2019-09-10 12:23:48 +0200

    Planets adding content to the card

--- lib/HomePageBody.dart
+++ lib/HomePageBody.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/PlanetRow.dart';
+import 'package:flutter_app/Planet.dart';
 
 
 class HomePageBody extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new PlanetRow();
+    return new PlanetRow(planets[1]);
   }
 }
