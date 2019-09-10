adf17fc3bc4b89c7aeeb21e2f9d9b10f32f2b83b
rspaimoc2 <raduspaimoc@gmail.com>
2019-09-10 11:19:06 +0200

    First commit

---
+++ lib/HomePageBody.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/PlanetRow.dart';
+
+
+class HomePageBody extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new PlanetRow();
+  }
+}
