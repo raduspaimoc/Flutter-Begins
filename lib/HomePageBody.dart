ec1188e695d3063004d8a2102774e9c9a7daf8cd
rspaimoc2 <raduspaimoc@gmail.com>
2019-09-10 12:48:54 +0200

    Sliver padding in list view added

--- lib/HomePageBody.dart
+++ lib/HomePageBody.dart
@@ -7,6 +7,25 @@ class HomePageBody extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new PlanetRow(planets[1]);
+    return new Expanded(
+        child: new Container(
+          color: new Color(0xFF736AB7),
+          child: new CustomScrollView(
+            scrollDirection: Axis.vertical,
+            slivers: <Widget>[
+              new SliverPadding(
+                padding: const EdgeInsets.symmetric(vertical: 24.0),
+                sliver: new SliverFixedExtentList(
+                  itemExtent: 152.0,
+                  delegate: new SliverChildBuilderDelegate(
+                      (context, index) => new PlanetRow(planets[index]),
+                    childCount: planets.length
+                  ),
+                ),
+              )
+            ],
+          ) ,
+        ),
+    );
   }
 }
